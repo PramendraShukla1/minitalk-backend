4ddc619cf5d67396dd24643e4d14a76cabf7ba94
Pramendra Shukla <shuklapramendra99@gmail.com>
2024-01-19 22:58:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "https://minitalk-pramendra-shukla.netlify.app/",
   })
 );
 
