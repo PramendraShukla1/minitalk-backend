d403c91d4caa249f517040313eb5d38d1bd21d69
Pramendra Shukla <shuklapramendra99@gmail.com>
2024-01-19 23:04:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://your-frontend-domain.com');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 //Cors
 app.use(
   cors({
